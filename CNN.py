a45b53eb3a2476bc468f7fc20dc48c39aad2fee4
Ben <ben.hevrony@gmail.com>
2021-08-13 16:51:09 +0300

    CNN example file

---
+++ CNN.py
@@ -0,0 +1,9 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras import backend as K
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+print(x_train.shape, y_train.shape)
