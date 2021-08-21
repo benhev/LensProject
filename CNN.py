07b68bd4989b22899658205e764c94ad6c93d5ee
Ben <ben.hevrony@gmail.com>
2021-08-21 18:24:04 +0300

    code fixed to allow different models to be saved changed the model layers to have a maxpool layer between the Conv2D layers reduced dense layer from 256 to 128

--- CNN.py
+++ CNN.py
@@ -1,4 +1,6 @@
-batch_size = 128
+import numpy as np
+
+batch_size = 32
 num_classes = 10
 epochs = 10
 
@@ -9,8 +11,15 @@ from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 from keras import backend as K
 import tensorflow as tf
 from PIL import Image
+from tensorflow.keras.callbacks import TensorBoard
+
+
+ver=3
+NAME = 'digit_recog_v{}.h5'.format(ver)
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+tb = TensorBoard(log_dir='logs/{}'.format(NAME))
 
 print(x_train.shape, y_train.shape)
 
@@ -18,15 +27,18 @@ x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 input_shape = (28, 28, 1)
 
-print(y_train.shape)
+# print(y_train.shape)
 
 y_train = tf.keras.utils.to_categorical(y_train, num_classes=num_classes)
 y_test = tf.keras.utils.to_categorical(y_test, num_classes=num_classes)
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+# x_train = x_train.astype('float32')
+# x_test = x_test.astype('float32')
+# x_train /= 255
+# x_test /= 255
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
@@ -34,23 +46,22 @@ print(x_test.shape[0], 'test samples')
 
 # MODEL
 
-# model = Sequential()
-# model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-# model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-# model.add(MaxPool2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
-# model.add(Flatten())
-# model.add(Dense(256, activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(num_classes, activation='softmax'))
-#
-# model.compile(loss=tf.keras.losses.categorical_crossentropy, optimizer=tf.keras.optimizers.Adadelta(),
-#               metrics=['accuracy'])
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
 
-model = tf.keras.models.load_model('mnist.h5')
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+model.compile(loss='categorical_crossentropy', optimizer='Adadelta',
+              metrics=['accuracy'])
 
-model.save('mnist2.h5')
+# model = tf.keras.models.load_model('mnist.h5')
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test), callbacks=tb)
+
+model.save('models/{}'.format(NAME))
 print('The model has successfully trained.')
-
-
