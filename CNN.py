1d64d8e3c15f0f62e82c0dbda9dc507124bca7e3
Ben <ben.hevrony@gmail.com>
2021-08-30 18:21:29 +0300

    Callbacks

--- CNN.py
+++ CNN.py
@@ -1,38 +1,41 @@
 import numpy as np
-# import keras
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 # from keras import backend as K
 import tensorflow as tf
 # from PIL import Image
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from os.path import isdir
 
-batch_size = 30 #20
+batch_size = 30  # 20
 num_classes = 10
 epochs = 50
 input_shape = (28, 28, 1)
 print('Model version: ')
 ver = input()
-NAME = 'digit_recog_v'+ver
-while isdir('models/'+NAME):
+NAME = 'digit_recog_v' + ver
+while isdir('models/' + NAME):
     print('Model version exists. Enter different version: ')
     ver = input()
-    NAME = 'digit_recog_v'+ver
+    NAME = 'digit_recog_v' + ver
 
 print(NAME)
 
+### Callbacks ###
+
+tb = TensorBoard(log_dir='logs/' + NAME)  # TensorBoard
+mcp = ModelCheckpoint(filepath='models/' + NAME + '/Checkpoint)', save_freq='epoch', verbose=1)  # Model Checkpoint
+mbst = ModelCheckpoint(filepath='models/' + NAME + '/BestFit', monitor='val_loss',
+                       save_best_only=True, verbose=1)  # Best Model Checkpoint
+estop = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=3, mode='min', verbose=1)
+redlr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, verbose=1, mode='min', min_delta=1e-4)
+
+### End Callbacks ###
+
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-tb = TensorBoard(log_dir='logs/'+NAME)
-
-# print(x_train.shape, y_train.shape)
-
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-
-# print(y_train.shape)
 
 y_train = tf.keras.utils.to_categorical(y_train, num_classes=num_classes)
 y_test = tf.keras.utils.to_categorical(y_test, num_classes=num_classes)
@@ -45,10 +48,6 @@ y_test = tf.keras.utils.to_categorical(y_test, num_classes=num_classes)
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-# print('x_train shape:', x_train.shape)
-# print(x_train.shape[0], 'train samples')
-# print(x_test.shape[0], 'test samples')
-
 # MODEL
 
 model = Sequential()
@@ -56,10 +55,10 @@ model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=input_sh
 model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPool2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
+model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-# model.add(Dropout(0.5))
+model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='Adadelta',
@@ -67,7 +66,7 @@ model.compile(loss='categorical_crossentropy', optimizer='Adadelta',
 
 # model = tf.keras.models.load_model('mnist.h5')
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test),
-          callbacks=tb)
+          callbacks=[tb, mcp, mbst, redlr])
 
 model.save('models/' + NAME)
 print('The model has successfully trained.')
