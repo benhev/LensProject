448433ea2f1e1a43b45685e97295bcb6e8e0c084
Ben <ben.hevrony@gmail.com>
2021-09-03 17:50:09 +0300

    creation of Lens CNN created import data function to properly import the test set

--- Lens CNN.py
+++ Lens CNN.py
@@ -1,5 +1,3 @@
-import time
-
 import numpy as np
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
@@ -15,81 +13,103 @@ import lzma
 import pickle
 
 
+def time_convert(seconds):
+    hrs = str(np.floor(seconds / 3600).astype('int'))
+    seconds = np.mod(seconds, 3600)
+    mins = str(np.floor(seconds / 60).astype('int'))
+    seconds = str(np.mod(seconds, 60).astype('int'))
+    if len(hrs) == 1:
+        hrs = '0' + hrs
+    if len(mins) == 1:
+        mins = '0' + mins
+    if len(seconds) == 1:
+        seconds = '0' + seconds
+    return ':'.join([hrs, mins, seconds])
+
+
 def file_read(directory):
     with lzma.open(directory, 'rb') as file:
         loaded_data = pickle.load(file)
     return loaded_data
 
 
-def time_convert(seconds):
-    hrs = str(np.floor(seconds / 3600).astype('int'))
-    seconds = np.mod(seconds, 3600)
-    mins = str(np.floor(seconds / 60).astype('int'))
-    seconds = str(np.mod(seconds, 60).astype('int'))
-    return ':'.join([hrs, mins, seconds])
+def import_data(directory: str, stack=10000, img_shape=(100, 100)):
+    num_files = len(listdir(directory))
+    x, y = np.zeros(shape=((num_files - 1) * stack, img_shape[0], img_shape[1], 1)), np.zeros(
+        shape=((num_files - 1) * stack, img_shape[0], img_shape[1]))
+    ind = 0
+    for file in listdir(directory):
+        X, Y = file_read(directory + "/" + file)
+
+        assert isinstance(X, np.ndarray) and isinstance(Y, np.ndarray)
+        assert len(X) == stack
+        assert X.shape == Y.shape
+
+        if len(Y.shape) > 3:
+            Y = Y.reshape(shape=Y.shape[:-1])
+
+        if ind < (num_files - 1) * stack:
+            for i in range(stack):
+                x[ind + i] = X[i]  # / X[i].max()
+                y[ind + i] = Y[i]
+            ind += len(X)
+
+    # for i in range(len(X)):
+    #     X[i] = X[i] / X[i].max()
+
+    return (x, y), (X, Y)
 
 
-# batch_size = 30  # 20
-# # num_classes = 10
-# epochs = 1
-# input_shape = (28, 28, 1)
-# print('Model version: ')
-# ver = input()
-# NAME = 'Lens_CNN_v' + ver
-# while isdir('models/' + NAME):
-#     print('Model version exists. Enter different version: ')
-#     ver = input()
-#     NAME = 'Lens_CNN_v' + ver
-#
-# print(NAME)
-#
-# for file in listdir('Training Set'):
-#     file_read('Training Set/'+file)
-t0 = time.time()
-print('Reading file...')
-x = file_read("Training Set/lens_set_1.xz")
-t1 = time.time()
-print('Time Elapsed ' + time_convert(t1 - t0))
+batch_size = 30  # 20
+# num_classes = 10
+epochs = 1
+input_shape = (100, 100, 1)
+print('Model version: ')
+ver = input()
+NAME = 'Lens_CNN_v' + ver
+while isdir('models/' + NAME):
+    print('Model version exists. Enter different version: ')
+    ver = input()
+    NAME = 'Lens_CNN_v' + ver
 
+print('Building model ' + NAME)
 
-# ### Callbacks ###
-#
-# tb = TensorBoard(log_dir='logs/' + NAME)  # TensorBoard
-# mcp = ModelCheckpoint(filepath='models/' + NAME + '/Checkpoint)', save_freq='epoch', verbose=1)  # Model Checkpoint
-# mbst = ModelCheckpoint(filepath='models/' + NAME + '/BestFit', monitor='val_loss',
-#                        save_best_only=True, verbose=1)  # Best Model Checkpoint
-# estop = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=3, mode='min', verbose=1)  # Early Stopping
-# redlr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, verbose=1, mode='min',
-#                           min_delta=1e-4)  # Adaptive Learning Rate
-#
-# ### End Callbacks ###
+### Callbacks ###
 
+tb = TensorBoard(log_dir='logs/' + NAME)  # TensorBoard
+mcp = ModelCheckpoint(filepath='models/' + NAME + '/Checkpoint.h5', save_freq='epoch', verbose=1)  # Model Checkpoint
+mbst = ModelCheckpoint(filepath='models/' + NAME + '/BestFit.h5', monitor='val_loss',
+                       save_best_only=True, verbose=1)  # Best Model Checkpoint
+estop = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=3, mode='min', verbose=1)  # Early Stopping
+redlr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, verbose=1, mode='min',
+                          min_delta=1e-4)  # Adaptive Learning Rate
 
-# y_train = tf.keras.utils.to_categorical(y_train, num_classes=num_classes)
-# y_test = tf.keras.utils.to_categorical(y_test, num_classes=num_classes)
+(x_train, y_train), (x_test, y_test) = import_data(directory='Temp Training Set', stack=100, img_shape=(100, 100))
 
-# x_train = tf.keras.utils.normalize(x_train, axis=1)
-# x_test = tf.keras.utils.normalize(x_test, axis=1)
+### End Callbacks ###
 
-# ### MODEL ###
-#
-# model = Sequential()
-# model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-# model.add(MaxPool2D(pool_size=(2, 2)))
-# model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-# model.add(MaxPool2D(pool_size=(2, 2)))
-# # model.add(Dropout(0.25))
-# model.add(Flatten())
-# model.add(Dense(128, activation='relu'))
-# # model.add(Dropout(0.5))
-# model.add(Dense(num_classes, activation='softmax'))
-#
-# model.compile(loss='categorical_crossentropy', optimizer='Adadelta',
-#               metrics=['accuracy'])
-#
-# # model = tf.keras.models.load_model('mnist.h5')
-# model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test),
-#           callbacks=[tb, mcp, mbst, redlr])
-#
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+### MODEL ###
+
+model = Sequential()
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='Adadelta',
+              metrics=['accuracy'])
+
+# model = tf.keras.models.load_model('mnist.h5')
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test),
+          callbacks=[tb, mcp, mbst, redlr])
+
 # model.save('models/' + NAME)
-# print('The model has successfully trained.')
+print('The model has successfully trained.')
