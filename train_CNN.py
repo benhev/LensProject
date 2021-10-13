b9ac5f17ee7705acb6af197b7e6e0fb584bb7be3
Ben <ben.hevrony@gmail.com>
2021-10-13 17:44:09 +0300

    changes to train_cnn

--- train_CNN.py
+++ train_CNN.py
@@ -2,6 +2,6 @@ from lensing_simulation import simulation
 from LensCNN import create_cnn
 
 if __name__ == '__main__':
-    training_dir = simulation(npix=150, deltapix=0.1, stacks=10, stack_size=10000, action='save')
-    create_cnn(model_name='Alpha', batch_size=100, epochs=3, training_dir='test training', callback=['mbst'],
+    training_dir = simulation(npix=152, deltapix=0.1, stacks=10, stack_size=10000, action='save')
+    create_cnn(model_name='Alpha', batch_size=100, epochs=3, training_dir='test training', callback=['tb','mbst'],
                comments='automated run')
